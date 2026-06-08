599984a97b7892f65cec84ba5541b5fe9cda3a34
arthur medeiros <medeiros.arthur13@escola.pr.gov.br>
2026-06-08 11:56:48 -0300

    Add files via upload

--- telaGerente.py
+++ telaGerente.py
@@ -1,6 +1,7 @@
-import tkinter
+import tkinter as tk
 from conta import Conta
 import json
+
 def cadastrar():
     agencia = input_agencia.get()
     titular = input_titular.get()
@@ -24,4 +25,39 @@ def cadastrar():
     # resposta de sucesso na tela
     label_resposta.configure(
         text=f"Conta: {nova_conta.numero} Titular: {nova_conta.titular} cadastrado com sucesso!",
-        fg="green")
+        fg="green") 
+
+
+app = tk.Tk()
+app.title("Banco DSB - Gerente")
+app.geometry("400x300") # Aumentado levemente para acomodar o título com folga
+
+# TÍTULO DA TELA
+label_titulo = tk.Label(app, text="Cadastro de clientes", font=("Arial", 16, "bold"))
+label_titulo.pack(pady=15)
+
+# AGÊNCIA
+label_agencia = tk.Label(app, text="Agência:")
+label_agencia.pack(pady=5)
+input_agencia = tk.Entry(app)
+input_agencia.pack()
+
+# TITULAR
+label_titular = tk.Label(app, text="Titular:")
+label_titular.pack(pady=5)
+input_titular = tk.Entry(app)
+input_titular.pack()
+
+# CPF
+label_cpf = tk.Label(app, text="CPF:")
+label_cpf.pack(pady=5)
+input_cpf = tk.Entry(app)
+input_cpf.pack()
+
+btn_enviar = tk.Button(app, text="Cadastrar Cliente", command=cadastrar)
+btn_enviar.pack(pady=10)
+
+label_resposta = tk.Label(app, text="")
+label_resposta.pack(pady=5)
+
+app.mainloop()
